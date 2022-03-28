70032f1595812722d4f2fa8f12dcfd1d946e6a53
Lupusatris <claudeblandin@yahoo.fr>
2022-03-28 14:53:01 +0200

    ajout fichier

--- exerciceArticle/connectBDD.php
+++ exerciceArticle/connectBDD.php
@@ -1,4 +1,4 @@
 <?php
-$bdd = new PDO('mysql:host=localhost;dbname=nom_de_la_bdd', 'root','', 
-array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+    $bdd = new PDO('mysql:host=localhost;dbname=produits', 'root','', 
+    array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
 ?>
